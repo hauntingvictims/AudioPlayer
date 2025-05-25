448544c64e3ccc03d3ce4d957365be5767b02a68
Mr.Robot <robot.vis@gmail.com>
2025-05-25 16:58:30 +0300

    added numerating for song list, icon for play etc fixed

---
+++ NumberConverter.cs
@@ -0,0 +1,25 @@
+﻿using Avalonia.Data.Converters;
+using System;
+using System.Globalization;
+using System.Collections;
+
+namespace AudioPlayer
+{
+    public class NumberConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null && parameter is IList collection)
+            {
+                int index = collection.IndexOf(value);
+                return $"{index + 1:D2}. "; 
+            }
+            return "01.";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
